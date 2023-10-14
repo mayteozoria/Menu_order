46f2666560ff4842134727f0b1105a55a98a6e93
Mayte Ozoria <mayteozoria@gmail.com>
2023-10-14 13:14:43 -0400

    Initial Commit

---
+++ src/app/menu/[items]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemPage = () => {
+  return (
+    <div>
+      Items 
+    </div>
+  )
+}
+
+export default ItemPage
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      SingleProduct
+    </div>
+  )
+}
+
+export default Product
